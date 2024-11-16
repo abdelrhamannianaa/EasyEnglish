0ef73c50395e1766fd2e6bc2c874e3dbd1c395f4
danaya8 <158086166+danaya8@users.noreply.github.com>
2024-11-16 20:46:53 +0000

    Gives random sentence

---
+++ randomWordGenerator/randomWord.py
@@ -0,0 +1,10 @@
+import random
+
+def randomWord():
+    content = []
+
+    with open("sentences.txt", "r") as word:
+        content = word.read().splitlines()
+
+    chose = random.choice(content)
+    return chose
