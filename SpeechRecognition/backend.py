51baa332ca6881a8116c701f9b37a955a1a32392
Abdelrhaman Nianaa <anianaa@student.cccd.edu>
2024-11-16 16:15:06 -0800

    most recent changes to all

--- SpeechRecognition/backend.py
+++ SpeechRecognition/backend.py
@@ -1,29 +1,64 @@
 import difflib
 import random
-def tryout(wordSpoken, correctPhrase):
+import speech_recognition
+import pyttsx3
+import os
+
+def tryout(correctPhrase, wordSpoken):
     lowerChoice = wordSpoken.lower()
     lowerAnswer = correctPhrase.lower()
 
     similarity = difflib.SequenceMatcher(None, lowerChoice, lowerAnswer).ratio()
 
-    if similarity >= 0.9:
+    if similarity >= 0.8:
         return True
     else:
         return False
-    
-with open('output.txt', 'r') as file:
-    userInput = file.readline()
+
+def userInput(oFilepath):
+    with open(r'C:\Users\a_nie\fnChck\output.txt', 'r') as file:
+        userInput = file.readline()
+    return userInput
 
 
-with open('sentences.txt', 'r') as file:
-    correctSentences = file.readlines()
+def chosenPhrase():
+    with open(r'C:\Users\a_nie\fnChck\EasyEnglish\SpeechRecognition\sentences.txt', 'r') as file:
+        correctSentences = file.readlines()
 
-chosenPhrase = random.choice(correctSentences)
+    chosenPhrase = random.choice(correctSentences)
+    return chosenPhrase
 
-if(tryout(userInput, "vincent is not") == True):
-        print("Correct!")
-else:
-    print("Incorrect!")
+
+def speechRecognition():
+    recognizer = speech_recognition.Recognizer()
+    outfile = open("output.txt", "w")
+    try:
+
+        with speech_recognition.Microphone() as mic:
+
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+
+            print(f"{text}")
+
+            # if "exit" in text:
+            #     print("Exiting...")
+            #     run = False
+            outfile.write(text)
+            outfile.write("\n")
+
+    except speech_recognition.UnknownValueError:
+        print("Please say again!")
+        recognizer = speech_recognition.Recognizer()
+
+    except speech_recognition.RequestError:
+        print("could not request result; {0}".format(speech_recognition.RequestError))
+        recognizer = speech_recognition.Recognizer()
+        
+
 
 
 
--- SpeechRecognition/main.py
+++ SpeechRecognition/main.py
@@ -1,33 +1,18 @@
-import speech_recognition
-import pyttsx3
 
-recognizer = speech_recognition.Recognizer()
-outfile = open("output.txt", "w")
-run = True
+from backend import *
 
 
-try:
 
-    with speech_recognition.Microphone() as mic:
 
-        recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-        audio = recognizer.listen(mic)
 
-        text = recognizer.recognize_google(audio)
-        text = text.lower()
+print("your chosen word is ")
+chosenWord = chosenPhrase()
+oFilepath = input("Please input where the output file is located: ")
+print(f"the phrase to repeat is: {chosenWord}")
 
-        print(f"Recognized {text}")
 
-        # if "exit" in text:
-        #     print("Exiting...")
-        #     run = False
-        outfile.write(text)
-        outfile.write("\n")
+speechRecognition()
 
-except speech_recognition.UnknownValueError:
-    print("Please say again!")
-    recognizer = speech_recognition.Recognizer()
+print(tryout(chosenWord,userInput(oFilepath)))
 
-except speech_recognition.RequestError:
-    print("could not request result; {0}".format(speech_recognition.RequestError))
-    recognizer = speech_recognition.Recognizer()
+
