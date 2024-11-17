f22f5b56a7156dc2460918d15d981480ddd06bb4
Abdelrhaman Nianaa <anianaa@student.cccd.edu>
2024-11-16 16:52:24 -0800

    for help

--- SpeechRecognition/main.py
+++ SpeechRecognition/main.py
@@ -2,12 +2,14 @@
 from backend import *
 
 
-
-
+#C:\Users\a_nie\fnChck\EasyEnglish\SpeechRecognition\sentences.txt
+#C:\Users\a_nie\fnChck\EasyEnglish\output.txt
 
 print("your chosen word is ")
-chosenWord = chosenPhrase()
+iFilePath = input("Please enter where you will be pulling the phrase from: ")
+chosenWord = chosenPhrase(iFilePath)
 oFilepath = input("Please input where the output file is located: ")
+print(oFilepath)
 print(f"the phrase to repeat is: {chosenWord}")
 
 
